3d458d733119cef058e831ebf5fff43ef037813f
cola99year <1620921710@qq.com>
2022-07-26 20:59:15 +0800

    cola_密码加密存储测试成功！

--- src/test/java/com/cola/sangeng_security/SangengSecurityApplicationTests.java
+++ src/test/java/com/cola/sangeng_security/SangengSecurityApplicationTests.java
@@ -5,6 +5,7 @@ import com.cola.sangeng_security.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
@@ -18,6 +19,13 @@ class SangengSecurityApplicationTests {
     public void testUserMapper(){
         List<User> users = userMapper.selectList(null);
         System.out.println(users);
+    }
+    @Test
+    public void TestBCryptPasswordEncoder(){
+        //查看括号内的明文加密后是:$2a$10$wJHiIzNfkyq.rb/FrTbrZe2PfGtc075z2sWRLoDKzJ.3Vu9mG0tO2
+        System.out.println(new BCryptPasswordEncoder().encode("123"));
+        //对比明文和密文是否匹配，若匹配则为True
+        System.out.println(new BCryptPasswordEncoder().matches("123","$2a$10$wJHiIzNfkyq.rb/FrTbrZe2PfGtc075z2sWRLoDKzJ.3Vu9mG0tO2"));
     }
 
 }
