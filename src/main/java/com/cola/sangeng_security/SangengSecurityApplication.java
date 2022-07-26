e814e6d3579d53094653a7fe9d8f4b864f3b307e
cola99year <1620921710@qq.com>
2022-07-26 15:09:31 +0800

    cola_init

---
+++ src/main/java/com/cola/sangeng_security/SangengSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cola.sangeng_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangengSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SangengSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cola/sangeng_security/SangengSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cola.sangeng_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangengSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
